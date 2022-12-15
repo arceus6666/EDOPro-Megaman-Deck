cd2f939292ad1b23b0b13dd4df1cd3f7c75d2266
Daniel Mendoza <arceus6666@hotmail.com>
2022-12-15 03:51:49 -0400

    modifed pantheon aqua

--- script/c666000032.lua
+++ script/c666000032.lua
@@ -25,9 +25,19 @@ function s.initial_effect(c)
   e2:SetOperation(s.e2Operation)
   c:RegisterEffect(e2)
 
-  s.listed_names = { PANTHEON_AQUA }
-
+  --indestructible reploid
+  local e3 = Effect.CreateEffect(c)
+  e3:SetType(EFFECT_TYPE_FIELD)
+  e3:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+  e3:SetRange(LOCATION_MZONE)
+  e3:SetTargetRange(LOCATION_MZONE, 0)
+  e3:SetTarget(aux.TargetBoolFunction(Card.IsSetCard, ARCHETYPES.REPLOID))
+  e3:SetValue(1)
+  c:RegisterEffect(e3)
 end
+
+s.listed_names = { PANTHEON_AQUA }
+s.listed_series = { ARCHETYPES.REPLOID }
 
 function s.filter(c, e, tp)
   return c:IsCode(PANTHEON_AQUA) and c:IsCanBeSpecialSummoned(e, 0, tp, true, true)
