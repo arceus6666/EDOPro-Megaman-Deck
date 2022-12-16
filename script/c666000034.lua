7d1ef33d9585c44adcbc5cb51fb5eda970587bde
Daniel Mendoza <arceus6666@hotmail.com>
2022-12-16 00:25:07 -0400

    new: omega (temp)

--- script/c666000034.lua
+++ script/c666000034.lua
@@ -5,26 +5,24 @@ local s, id = GetID()
 
 function s.initial_effect(c)
   --Activate
-  Ritual.AddProcEqual{handler=c, filter=s.ritualfil, extrafil=s.extrafil, extratg=s.extratg}
-  -- Ritual.AddProcGreaterCode{handler=c, filter=s.ritualfil, extrafil=s.extrafil, extratg=s.extratg}
+  Ritual.AddProcEqual { handler = c, filter = s.ritualfil, extrafil = s.extrafil, extratg = s.extratg }
 end
 
-s.listed_names={OMEGA}
+s.listed_names = { OMEGA, MEGAMAN_ZERO }
 
 function s.ritualfil(c)
-	return c:IsCode(OMEGA) and c:IsRitualMonster()
+  return c:IsCode(OMEGA) and c:IsRitualMonster()
 end
 
 function s.mfilter(c)
-	return not Duel.IsPlayerAffectedByEffect(c:GetControler(),69832741) and c:HasLevel() and c:IsCode(MEGAMAN_ZERO)
-		and c:IsMonster() and c:IsAbleToRemove()
+  return not Duel.IsPlayerAffectedByEffect(c:GetControler(), 69832741) and c:IsCode(MEGAMAN_ZERO) and c:IsAbleToRemove()
 end
 
-function s.extrafil(e,tp,eg,ep,ev,re,r,rp,chk)
-	return Duel.GetMatchingGroup(s.mfilter,tp,LOCATION_GRAVE,0,nil)
+function s.extrafil(e, tp, eg, ep, ev, re, r, rp, chk)
+  return Duel.GetMatchingGroup(s.mfilter, tp, LOCATION_GRAVE, 0, nil)
 end
 
-function s.extratg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,tp,LOCATION_GRAVE)
+function s.extratg(e, tp, eg, ep, ev, re, r, rp, chk)
+  if chk == 0 then return true end
+  Duel.SetOperationInfo(0, CATEGORY_REMOVE, nil, 1, tp, LOCATION_GRAVE)
 end
