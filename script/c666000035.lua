c69deca9efddf31698e91b2d7ff6ec0d60c86a98
Daniel Mendoza <arceus6666@hotmail.com>
2022-12-16 00:19:08 -0400

    new: omega (temp)

--- script/c666000035.lua
+++ script/c666000035.lua
@@ -5,4 +5,18 @@ local s, id = GetID()
 
 function s.initial_effect(c)
   c:EnableReviveLimit()
+
+  --Immune
+  local e1 = Effect.CreateEffect(c)
+  e1:SetType(EFFECT_TYPE_SINGLE)
+  e1:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+  e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+  e1:SetRange(LOCATION_MZONE)
+  e1:SetValue(1)
+  c:RegisterEffect(e1)
+
+  local e2 = e1:Clone()
+  e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+  e2:SetValue(1)
+  c:RegisterEffect(e2)
 end
