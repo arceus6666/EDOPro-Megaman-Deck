fae1d0252be4c648febcd0f72561f71115da457a
Daniel Mendoza <arceus6666@hotmail.com>
2022-12-22 18:19:05 -0400

    new: hacker elf

---
+++ script/c666000039.lua
@@ -0,0 +1,44 @@
+-- hacker elf
+
+Duel.LoadScript("functions.lua")
+local s, id = GetID()
+
+function s.initial_effect(c)
+  --Activate
+  local e1 = Effect.CreateEffect(c)
+  e1:SetCategory(CATEGORY_TODECK)
+  e1:SetType(EFFECT_TYPE_ACTIVATE)
+  e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+  e1:SetTarget(s.e1Target)
+  e1:SetOperation(s.e1Operation)
+  c:RegisterEffect(e1)
+end
+
+function s.filter(c, tp)
+  return not c:IsSummonPlayer(tp) and
+      (c:GetSummonLocation() & LOCATION_HAND + LOCATION_EXTRA) ~= 0 and
+      c:IsAbleToDeck() and
+      c:IsLocation(LOCATION_MZONE)
+end
+
+function s.e1Target(e, tp, eg, ep, ev, re, r, rp, chk)
+  local g = eg:Filter(s.filter, nil, tp)
+  local ct = #g
+  if chk == 0 then return ct > 0 end
+  Duel.SetTargetCard(eg)
+  Duel.SetOperationInfo(0, CATEGORY_TODECK, g, ct, 0, 0)
+end
+
+function s.e1Operation(e, tp, eg, ep, ev, re, r, rp)
+  local g = eg:Filter(s.filter, nil, tp):Filter(Card.IsRelateToEffect, nil, e)
+  if #g > 0 then
+    Duel.SendtoDeck(g, nil, SEQ_DECKSHUFFLE, REASON_EFFECT)
+    local og = Duel.GetOperatedGroup()
+    local ct = og:FilterCount(Card.IsLocation, nil,
+      LOCATION_DECK + LOCATION_EXTRA)
+    if ct > 0 then
+      Duel.BreakEffect()
+      Duel.SetLP(tp, Duel.GetLP(tp) + ct * 100)
+    end
+  end
+end
