1032d84b9938d569803691a66c6b7d1dbe60ebb0
Daniel Mendoza <arceus6666@hotmail.com>
2022-12-28 01:44:51 -0400

    new: prea

--- script/c666000053.lua
+++ script/c666000053.lua
@@ -7,72 +7,41 @@ function s.initial_effect(c)
   --pendulum summon
   Pendulum.AddProcedure(c)
 
-  --to hand
+  --pierce
   local e1 = Effect.CreateEffect(c)
-  e1:SetCategory(CATEGORY_TOHAND + CATEGORY_SEARCH)
-  e1:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_TRIGGER_O)
-  e1:SetCode(EVENT_BATTLED)
+  e1:SetType(EFFECT_TYPE_FIELD)
+  e1:SetCode(EFFECT_PIERCE)
   e1:SetRange(LOCATION_PZONE)
-  e1:SetCountLimit(1)
-  e1:SetCondition(s.e1Condition)
+  e1:SetTargetRange(LOCATION_MZONE, 0)
   e1:SetTarget(s.e1Target)
-  e1:SetOperation(s.e1Operation)
   c:RegisterEffect(e1)
 
-  --cannot be material
+  --on destroyed
   local e2 = Effect.CreateEffect(c)
-  e2:SetType(EFFECT_TYPE_SINGLE)
-  e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
-  e2:SetCode(EFFECT_CANNOT_BE_FUSION_MATERIAL)
-  e2:SetValue(s.e2Value)
+  e2:SetDescription(aux.Stringid(id, 1))
+  e2:SetCategory(CATEGORY_DESTROY)
+  e2:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
+  e2:SetCode(EVENT_BATTLE_DESTROYED)
+  e2:SetTarget(s.e2Target)
+  e2:SetOperation(s.e2Operation)
   c:RegisterEffect(e2)
-
-  local e3 = e2:Clone()
-  e3:SetCode(EFFECT_CANNOT_BE_SYNCHRO_MATERIAL)
-  c:RegisterEffect(e3)
-
-  local e4 = e2:Clone()
-  e4:SetCode(EFFECT_CANNOT_BE_XYZ_MATERIAL)
-  c:RegisterEffect(e4)
 end
 
-s.listed_series = { ARCHETYPES.CYBER_ELF }
+-- s.listed_series = { ARCHETYPES.CYBER_ELF }
 
-function s.e1Condition(e, tp, eg, ep, ev, re, r, rp)
-  local a = Duel.GetAttacker()
-  local d = Duel.GetAttackTarget()
-  if not d then return false end
-  if d:IsControler(tp) then a, d = d, a end
-  return a:IsType(TYPE_NORMAL) and
-      not a:IsStatus(STATUS_BATTLE_DESTROYED) and
-      d:IsStatus(STATUS_BATTLE_DESTROYED)
+function s.e1Target(e, c)
+  return c:IsType(TYPE_FUSION)
 end
 
-function s.filter(c)
-  return c:IsType(TYPE_EFFECT) and
-      c:IsLevelBelow(5) and
-      c:IsAbleToHand()
+function s.e2Target(e, tp, eg, ep, ev, re, r, rp, chk)
+  local bc = e:GetHandler():GetBattleTarget()
+  if chk == 0 then return bc:IsRelateToBattle() end
+  Duel.SetOperationInfo(0, CATEGORY_DESTROY, bc, 1, 0, 0)
 end
 
-function s.e1Target(e, tp, eg, ep, ev, re, r, rp, chk)
-  if chk == 0 then
-    return Duel.IsExistingMatchingCard(s.filter, tp, LOCATION_DECK, 0, 1, nil)
+function s.e2Operation(e, tp, eg, ep, ev, re, r, rp)
+  local bc = e:GetHandler():GetBattleTarget()
+  if bc:IsRelateToBattle() then
+    Duel.Destroy(bc, REASON_EFFECT)
   end
-  Duel.SetOperationInfo(0, CATEGORY_TOHAND, nil, 1, tp, LOCATION_DECK)
-end
-
-function s.e1Operation(e, tp, eg, ep, ev, re, r, rp)
-  if not e:GetHandler():IsRelateToEffect(e) then return end
-  Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_ATOHAND)
-  local g = Duel.SelectMatchingCard(tp, s.filter, tp, LOCATION_DECK,
-    0, 1, 1, nil)
-  if #g > 0 then
-    Duel.SendtoHand(g, nil, REASON_EFFECT)
-    Duel.ConfirmCards(1 - tp, g)
-  end
-end
-
-function s.e2Value(e, c)
-  if not c then return false end
-  return not c:IsSetCard(ARCHETYPES.CYBER_ELF)
 end
