6f55d1c637358ae1e99142ce42f94a884d7fb478
Daniel Mendoza <arceus6666@hotmail.com>
2022-12-28 00:14:23 -0400

    new: crea

--- script/c666000052.lua
+++ script/c666000052.lua
@@ -1,59 +1,78 @@
---竜剣士ラスターP
-local s,id=GetID()
+-- crea
+
+Duel.LoadScript("functions.lua")
+local s, id = GetID()
+
 function s.initial_effect(c)
-	--pendulum summon
-	Pendulum.AddProcedure(c)
-	--to hand
-	local e2=Effect.CreateEffect(c)
-	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-	e2:SetType(EFFECT_TYPE_IGNITION)
-	e2:SetRange(LOCATION_PZONE)
-	e2:SetCountLimit(1)
-	e2:SetCondition(s.thcon)
-	e2:SetTarget(s.thtg)
-	e2:SetOperation(s.thop)
-	c:RegisterEffect(e2)
-	--cannot be material
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_SINGLE)
-	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e3:SetCode(EFFECT_CANNOT_BE_FUSION_MATERIAL)
-	e3:SetValue(s.splimit)
-	c:RegisterEffect(e3)
-	local e4=e3:Clone()
-	e4:SetCode(EFFECT_CANNOT_BE_SYNCHRO_MATERIAL)
-	c:RegisterEffect(e4)
-	local e5=e3:Clone()
-	e5:SetCode(EFFECT_CANNOT_BE_XYZ_MATERIAL)
-	c:RegisterEffect(e5)
+  --pendulum summon
+  Pendulum.AddProcedure(c)
+
+  --to hand
+  local e1 = Effect.CreateEffect(c)
+  e1:SetCategory(CATEGORY_TOHAND + CATEGORY_SEARCH)
+  e1:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_TRIGGER_O)
+  e1:SetCode(EVENT_BATTLED)
+  e1:SetRange(LOCATION_PZONE)
+  e1:SetCountLimit(1)
+  e1:SetCondition(s.e1Condition)
+  e1:SetTarget(s.e1Target)
+  e1:SetOperation(s.e1Operation)
+  c:RegisterEffect(e1)
+
+  --cannot be material
+  local e2 = Effect.CreateEffect(c)
+  e2:SetType(EFFECT_TYPE_SINGLE)
+  e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
+  e2:SetCode(EFFECT_CANNOT_BE_FUSION_MATERIAL)
+  e2:SetValue(s.e2Value)
+  c:RegisterEffect(e2)
+
+  local e3 = e2:Clone()
+  e3:SetCode(EFFECT_CANNOT_BE_SYNCHRO_MATERIAL)
+  c:RegisterEffect(e3)
+
+  local e4 = e2:Clone()
+  e4:SetCode(EFFECT_CANNOT_BE_XYZ_MATERIAL)
+  c:RegisterEffect(e4)
 end
-s.listed_series={0xc7}
-function s.thcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(nil,tp,LOCATION_PZONE,0,1,e:GetHandler())
+
+s.listed_series = { ARCHETYPES.CYBER_ELF }
+
+function s.e1Condition(e, tp, eg, ep, ev, re, r, rp)
+  local a = Duel.GetAttacker()
+  local d = Duel.GetAttackTarget()
+  if not d then return false end
+  if d:IsControler(tp) then a, d = d, a end
+  return a:IsType(TYPE_NORMAL) and
+      not a:IsStatus(STATUS_BATTLE_DESTROYED) and
+      d:IsStatus(STATUS_BATTLE_DESTROYED)
 end
-function s.thfilter(c,code)
-	return c:IsCode(code) and c:IsAbleToHand()
+
+function s.filter(c)
+  return c:IsType(TYPE_NORMAL) and
+      c:IsLevelAbove(4) and
+      c:IsAbleToHand()
 end
-function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local sc=Duel.GetFirstMatchingCard(nil,tp,LOCATION_PZONE,0,e:GetHandler())
-	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil,sc:GetOriginalCode()) end
-	Duel.SetTargetCard(sc)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,sc,1,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+
+function s.e1Target(e, tp, eg, ep, ev, re, r, rp, chk)
+  if chk == 0 then
+    return Duel.IsExistingMatchingCard(s.filter, tp, LOCATION_DECK, 0, 1, nil)
+  end
+  Duel.SetOperationInfo(0, CATEGORY_TOHAND, nil, 1, tp, LOCATION_DECK)
 end
-function s.thop(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
-	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)~=0 then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-		local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil,tc:GetOriginalCode())
-		if #g>0 then
-			Duel.SendtoHand(g,nil,REASON_EFFECT)
-			Duel.ConfirmCards(1-tp,g)
-		end
-	end
+
+function s.e1Operation(e, tp, eg, ep, ev, re, r, rp)
+  if not e:GetHandler():IsRelateToEffect(e) then return end
+  Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_ATOHAND)
+  local g = Duel.SelectMatchingCard(tp, s.filter, tp, LOCATION_DECK,
+    0, 1, 1, nil)
+  if #g > 0 then
+    Duel.SendtoHand(g, nil, REASON_EFFECT)
+    Duel.ConfirmCards(1 - tp, g)
+  end
 end
-function s.splimit(e,c)
-	if not c then return false end
-	return not c:IsSetCard(0xc7)
+
+function s.e2Value(e, c)
+  if not c then return false end
+  return not c:IsSetCard(ARCHETYPES.CYBER_ELF)
 end
