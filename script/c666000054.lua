0f0e56b2be00f1b34316fa5ed23c2ab66ce11d07
Daniel Mendoza <arceus6666@hotmail.com>
2022-12-29 14:04:55 -0400

    new: dark elf

--- script/c666000054.lua
+++ script/c666000054.lua
@@ -27,18 +27,28 @@ function s.initial_effect(c)
   c:RegisterEffect(e1)
 
   --cannot be destroyed
-  local e4 = Effect.CreateEffect(c)
-  e4:SetType(EFFECT_TYPE_SINGLE)
-  e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-  e4:SetRange(LOCATION_MZONE)
-  e4:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
-  e4:SetValue(1)
-  c:RegisterEffect(e4)
+  local e2 = Effect.CreateEffect(c)
+  e2:SetType(EFFECT_TYPE_SINGLE)
+  e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+  e2:SetRange(LOCATION_MZONE)
+  e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+  e2:SetValue(1)
+  c:RegisterEffect(e2)
 
+  --Special summon a cyber-elf
+  local e3 = Effect.CreateEffect(c)
+  e3:SetDescription(aux.Stringid(id, 0))
+  e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+  e3:SetType(EFFECT_TYPE_IGNITION)
+  e3:SetCountLimit(1, { id, 1 })
+  e3:SetRange(LOCATION_MZONE)
+  e3:SetTarget(s.e3Target)
+  e3:SetOperation(s.e3Operation)
+  c:RegisterEffect(e3)
 end
 
 s.material = { CYBER_ELFS.CREA, CYBER_ELFS.PREA }
-s.listed_names = { CYBER_ELFS.CREA, CYBER_ELFS.PREA, TOKENS.CYBER_ELF }
+s.listed_names = { CYBER_ELFS.CREA, CYBER_ELFS.PREA, TOKENS.CYBER_ELF, id }
 s.listed_series = { ARCHETYPES.CYBER_ELF }
 s.synchro_nt_required = 1
 
@@ -60,5 +70,32 @@ function s.e1Operation(e, tp, eg, ep, ev, re, r, rp)
     local tkn = Duel.GetRandomNumber(TOKENS.CYBER_ELF, TOKENS.CYBER_ELF + 4)
     local token = Duel.CreateToken(tp, tkn)
     Duel.SpecialSummon(token, 0, tp, tp, false, false, POS_FACEUP_DEFENSE)
+  end
+end
+
+function s.filter(c, e, tp)
+  -- return c:IsRace(RACE_DRAGON) and
+  -- not c:IsCode(id) and
+  return c:IsSetCard(ARCHETYPES.CYBER_ELF) and
+      c:IsCanBeSpecialSummoned(e, 0, tp, false, false)
+end
+
+function s.e3Target(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+  if chk == 0 then
+    return Duel.GetLocationCount(tp, LOCATION_MZONE) > 0 and
+        Duel.IsExistingMatchingCard(s.filter, tp,
+          LOCATION_GRAVE + LOCATION_HAND, 0, 1, nil, e, tp)
+  end
+  Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, nil, 1, tp,
+    LOCATION_GRAVE + LOCATION_HAND)
+end
+
+function s.e3Operation(e, tp, eg, ep, ev, re, r, rp)
+  if Duel.GetLocationCount(tp, LOCATION_MZONE) <= 0 then return end
+  Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_SPSUMMON)
+  local g = Duel.SelectMatchingCard(tp, aux.NecroValleyFilter(s.filter),
+    tp, LOCATION_GRAVE + LOCATION_HAND, 0, 1, 1, nil, e, tp)
+  if #g > 0 then
+    Duel.SpecialSummon(g, 0, tp, tp, false, false, POS_FACEUP)
   end
 end
