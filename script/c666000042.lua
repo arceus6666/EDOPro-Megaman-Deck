a6dff7c355de0c200f709a4e739d54c0596fb8ee
Daniel Mendoza <arceus6666@hotmail.com>
2022-12-31 20:17:18 -0400

    test: adding base 4 models

--- script/c666000042.lua
+++ script/c666000042.lua
@@ -29,7 +29,7 @@ function s.initial_effect(c)
 
   --Special summon regular aeolus from deck
   local e3 = Effect.CreateEffect(c)
-  e3:SetDescription(aux.Stringid(id, 0))
+  e3:SetDescription(aux.Stringid(id, 1))
   e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
   e3:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
   e3:SetCode(EVENT_BATTLE_DESTROYED)
