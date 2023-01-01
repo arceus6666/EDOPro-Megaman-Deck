259376b7bd62fef423b2258a4588312fb9dbe06e
Daniel Mendoza <arceus6666@hotmail.com>
2022-12-31 22:40:43 -0400

    new: adding base 4 models

--- script/c666000056.lua
+++ script/c666000056.lua
@@ -8,4 +8,20 @@ function s.initial_effect(c)
 
   --cannot special summon
   CannotSpecialSummon(c)
+
+  --disable effect
+  local e3 = Effect.CreateEffect(c)
+  e3:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_CONTINUOUS)
+  e3:SetCode(EVENT_CHAIN_SOLVING)
+  e3:SetRange(LOCATION_MZONE)
+  e3:SetOperation(s.disop)
+  c:RegisterEffect(e3)
+
+end
+
+function s.disop(e, tp, eg, ep, ev, re, r, rp)
+  local tl = Duel.GetChainInfo(ev, CHAININFO_TRIGGERING_LOCATION)
+  if (tl & LOCATION_SZONE) ~= 0 and re:IsActiveType(TYPE_SPELL) then
+    Duel.NegateEffect(ev)
+  end
 end
