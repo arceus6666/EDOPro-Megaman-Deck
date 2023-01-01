8d8c2b001b98ab33c86ca7a8e7c597c6ba486da6
Daniel Mendoza <arceus6666@hotmail.com>
2023-01-01 01:40:04 -0400

    new: adding base 4 models

--- script/c666000045.lua
+++ script/c666000045.lua
@@ -9,12 +9,12 @@ function s.initial_effect(c)
   Fusion.AddProcMix(c, true, true, PSEUDORIODS.LURERRE, PSEUDORIODS.LEGANCHOR)
 
   --spsummon condition
-  local e1 = Effect.CreateEffect(c)
-  e1:SetType(EFFECT_TYPE_SINGLE)
-  e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
-  e1:SetCode(EFFECT_SPSUMMON_CONDITION)
-  e1:SetValue(aux.fuslimit)
-  c:RegisterEffect(e1)
+  -- local e1 = Effect.CreateEffect(c)
+  -- e1:SetType(EFFECT_TYPE_SINGLE)
+  -- e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
+  -- e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+  -- e1:SetValue(aux.fuslimit)
+  -- c:RegisterEffect(e1)
 
   --search
   local e2 = Effect.CreateEffect(c)
